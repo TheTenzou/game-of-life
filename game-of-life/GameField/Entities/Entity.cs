b2d0473873edcf1d388136b83fe15631b479b91e
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-03-22 11:18:06 +0300

    Add enitity

---
+++ game-of-life/GameField/Entities/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_of_life.GameField.Entities
+{
+    class Entity
+    {
+        public Gender Gender { get; }
+
+        public int FoodAmount
+        {
+            get => FoodAmount;
+            set
+            {
+                if (value > 100) FoodAmount = 100;
+                else if (value < 1) FoodAmount = 0;
+                else FoodAmount = value;
+            }
+        }
+
+        public Point Destination { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
---
+++ game-of-life/GameField/Entities/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_of_life.GameField.Entities
+{
+    enum Status
+    {
+        DEAD,
+        ALIVE
+    }
+}
